0afb19f9b25574e299ba33cf77d5f358621feb0f
Dandan.Xue <tentenxue@gmail.com>
2021-07-07 16:18:50 +0200

    release test v1.1.1

--- update_peprmint-web.sh
+++ update_peprmint-web.sh
@@ -1,12 +1,13 @@
-
-# build the latest version
-
-mv index.html index_.html 
+### build the latest version
+rm -rf peprmint/*
 
 cd ../peprmint-web/web-client/
 
-npx webpack --mode production --no-watch -o ../../reuter-group.github.io/
+## replace router basename for host env., i.e. Github.io
+sed -i '' s/'<Router'/'<Router basename="\/peprmint"'/g src/code/components/BaseLayout.tsx
 
-cd - 
-mv index.html peprmint-web.html
-mv index_.html index.html 
+## build
+npx webpack -c github.io.webpack.config.js --no-watch -o ../../reuter-group.github.io/peprmint/
+ 
+## restore change
+git checkout src/code/components/BaseLayout.tsx
